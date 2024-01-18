b80df8279e1077481e31e4448ee65b3089201e14
Nick Savage <nick@artemis.lan>
2024-01-18 07:09:02 -0500

    Implementing flags

--- disassemble.c
+++ disassemble.c
@@ -10,6 +10,11 @@ static const int REG_L = 5;
 static const int REG_F = 6;
 static const int REG_A = 7;
 
+static const int FLAG_Z = 7;
+static const int FLAG_N = 6;
+static const int FLAG_H = 5;
+static const int FLAG_C = 4;
+
 static const char *regNames[] = {"b", "c", "d", "e", "h", "l", "f", "a"};
 int8_t memory[65535];
 int8_t regs[] = {0,0,0,0,0,0,0,0};
@@ -61,10 +66,23 @@ unsigned char* readFile(char *filename, size_t* size) {
     return buffer;
 };
 
+void sub(unsigned char amount) {
+    regs[REG_A] -= amount;
+    
+    if (regs[REG_A] == 0) {
+	regs[REG_F] |= 1 << FLAG_Z;
+    } else {
+	regs[REG_F] |= 0 << FLAG_Z;
+    }
+    regs[REG_F] |= 1 << FLAG_N;
+    
+}
+
 int parse_opcode(unsigned char first, unsigned char second, unsigned char third) {
     
     int ret = 0;
     if ((first & 0b11000111) == 0b00000110) {
+	// ld r, n
 	// load register immediate
 	printByteAsBinary(second);
 	int reg = (first & 0b00111000) >> 3;
@@ -80,6 +98,16 @@ int parse_opcode(unsigned char first, unsigned char second, unsigned char third)
 	printf(" ld a, [HL]");
 	regs[REG_A] = memory[addr];
     }
+    else if ((first & 0b11111000) == 0b01110000) {
+	// ld (HL), r
+	
+	int addr = (regs[REG_H] << 8) + regs[REG_L];
+	int src = (first & 0b00000111);
+	
+	printf(" ld [HL], %s", regNames[src]);
+
+	memory[addr] = regs[src];
+    }
     else if ((first & 0b11000000) == 0b01000000) {
 	// ld r, r
 	// load reg (reg)
@@ -88,12 +116,30 @@ int parse_opcode(unsigned char first, unsigned char second, unsigned char third)
 	printf(" ld %s, %s", regNames[dest], regNames[src]);
 	regs[dest] = regs[src];
     }
+    else if ((first & 0b11111111) == 0b00110110) {
+	// ld (HL), n
+
+	int addr = (regs[REG_H] << 8) + regs[REG_L];
+	
+	memory[addr] = second;
+	ret = 1;
+    }
+    else if ((first & 0b11111111) == 0b00001010) {
+	// ld a, (BC)
+	
+	int addr = (regs[REG_B] << 8) + regs[REG_C];
+	
+	regs[REG_A] = memory[addr];
+	ret = 1;
+    }
     else if ((first & 0b11111000) == 0b10000000) {
 	// add r
 	int reg = (first & 0b00111000) >> 3;
 	printf(" add %s", regNames[reg]);
 	
 	regs[REG_A] += regs[reg];
+
+	regs[REG_F] |= 0 << FLAG_N;
 	
     }
     else if ((first & 0b11111111) == 0b11000110) {
@@ -104,12 +150,15 @@ int parse_opcode(unsigned char first, unsigned char second, unsigned char third)
 	regs[REG_A] += second;
 	printf("-%d-", regs[REG_A]);
 	ret = 1;
+
+	regs[REG_F] |= 0 << FLAG_N;
     }
     else if ((first & 0b11111000) == 0b10010000) {
 	// sub r
 	int reg = (first & 0b00111000) >> 3;
-	printf(" sub a, %s", regNames[reg]);
-	regs[REG_A] -= regs[reg]; 
+ 	printf(" sub a, %s", regNames[reg]);
+	sub(regs[reg]);
+	
 	ret = 1;
     }
     else if ((first & 0b11111111) == 0b11010110) {
@@ -117,7 +166,7 @@ int parse_opcode(unsigned char first, unsigned char second, unsigned char third)
 	printByteAsBinary(second);
 	putchar(' ');
 	printf(" sub a, %d", second);
-	regs[REG_A] -= second;
+	sub(second);
 	ret = 1;
     }
     else if ((first & 0b11111111) == 0b00100010) {
@@ -212,8 +261,12 @@ int main(int argc, char *argv[]) {
 	i += parse_opcode(fileData[i], fileData[i + 1], fileData[i + 2]);
     }
     putchar('\n');
-    for (int i = 0; i < 8; i++) {
-	printf("%s: %d\n", regNames[i],regs[i]);
+    for (int i = 0; i < 8; i+=2) {
+	printf("%s%s:", regNames[i],regNames[i + 1]);
+	printByteAsBinary(regs[i]);
+	putchar(' ');
+	printByteAsBinary(regs[i+1]);
+	putchar('\n');
     }
     // Free the memory
     free(fileData);
